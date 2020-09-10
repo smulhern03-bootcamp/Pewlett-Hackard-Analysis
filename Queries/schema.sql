bc5f07feb7957776749afd374ff493a552450917
smulhern03-bootcamp <stephen.k.mulhern@gmail.com>
2020-09-09 19:52:16 -0600

    Module update.

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -6,19 +6,18 @@ CREATE TABLE departments (
      UNIQUE (dept_name)
 );
 
-CREATE TABLE employees(
-	emp_no int NOT NULL,
-	birth_date date NOT NULL,
-	first_name VARCHAR NOT NULL,
-	last_name VARCHAR NOT NULL,
-	gender VARCHAR NOT NULL,
-	hire_date DATE NOT NULL,
-	PRIMARY KEY (emp_no),
-	UNIQUE (emp_no)
+CREATE TABLE employees (
+   	 emp_no INT NOT NULL,
+     birth_date DATE NOT NULL,
+     first_name VARCHAR NOT NULL,
+     last_name VARCHAR NOT NULL,
+     gender VARCHAR NOT NULL,
+     hire_date DATE NOT NULL,
+     PRIMARY KEY (emp_no)
 );
 
 CREATE TABLE dept_manager (
-dept_no VARCHAR(4) NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
     emp_no INT NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
@@ -37,24 +36,20 @@ CREATE TABLE salaries (
 );
 
 CREATE TABLE dept_emp (
-	dept_no INT NOT NULL,
 	emp_no INT NOT NULL,
+	dept_no VARCHAR (4) NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	PRIMARY KEY (emp_no)
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
+	PRIMARY KEY (emp_no, dept_no)
 );
 
 CREATE TABLE titles (
 	emp_no INT NOT NULL,
-	titles VARCHAR NOT NULL,
+	title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no),
-	PRIMARY KEY (titles), 
-	PRIMARY KEY (from_date)
+	PRIMARY KEY (emp_no, title, from_date)
 );
-
-select * from departments
