acbbe4acb55bd7936000be10fd9f0f36e46e134f
smulhern03-bootcamp <65323180+smulhern03-bootcamp@users.noreply.github.com>
2020-09-09 22:50:07 -0600

    Final query code

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -5,23 +5,26 @@ select e.emp_no,
 		t.title,
 		t.from_date,
 		t.to_date
-INTO retire_titles
+INTO retirement_titles
 from employees as e
 INNER JOIN titles as t
 	on (e.emp_no=t.emp_no)
 WHERE e.birth_date BETWEEN '1952-01-01' AND '1955-12-31'
 ORDER BY e.emp_no;
 
+select emp_no, first_name, last_name, title
+from retire_titles;
+
 -- Use Dictinct with Orderby to remove duplicate rows
 SELECT DISTINCT ON (emp_no) emp_no, 
 first_name, 
 last_name, 
 title
-INTO retire_titles_edited
+INTO unique_titles
 FROM retire_titles
 ORDER BY emp_no, to_date DESC;
 
---Get counts of each employee title.
+--Get counts of employee titles.
 Select Count (title), title
 INTO retiring_titles
 From unique_titles
