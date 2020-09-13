ccc603f2a300cf151d106c26fb6a6bfd9cf36fad
smulhern03-bootcamp <65323180+smulhern03-bootcamp@users.noreply.github.com>
2020-09-13 13:52:32 -0600

    Update Employee_Database_challenge.sql

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -49,3 +49,38 @@ INNER JOIN titles as t
 WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
 AND (de.to_date = '9999-01-01')
 ORDER BY e.emp_no;
+
+--Deliverable 3
+SELECT DISTINCT ON (e.emp_no) e.emp_no, 
+		e.first_name, 
+		e.last_name,
+		e.birth_date,
+		de.from_date,
+		de.to_date,
+		t.title
+INTO current_enginners
+from employees as e
+INNER JOIN dept_emp as de
+	on (e.emp_no=de.emp_no)
+INNER JOIN titles as t
+	on (e.emp_no=t.emp_no)
+WHERE (e.birth_date BETWEEN '1956-01-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01' AND t.title LIKE '% Engineer')
+ORDER BY e.emp_no;
+
+SELECT DISTINCT ON (e.emp_no) e.emp_no, 
+		e.first_name, 
+		e.last_name,
+		e.birth_date,
+		de.from_date,
+		de.to_date,
+		t.title
+INTO current_staff
+from employees as e
+INNER JOIN dept_emp as de
+	on (e.emp_no=de.emp_no)
+INNER JOIN titles as t
+	on (e.emp_no=t.emp_no)
+WHERE (e.birth_date BETWEEN '1956-01-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01' AND t.title LIKE '% Staff')
+ORDER BY e.emp_no;
