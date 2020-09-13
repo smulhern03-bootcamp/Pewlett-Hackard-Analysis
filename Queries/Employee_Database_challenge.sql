c70f76930617c652012a1971f0e321a669c10009
smulhern03-bootcamp <65323180+smulhern03-bootcamp@users.noreply.github.com>
2020-09-13 11:19:31 -0600

    Deliverable 2 added

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -30,3 +30,21 @@ INTO retiring_titles
 From unique_titles
 GROUP BY title
 ORDER BY count DESC;
+
+Deliverable 2
+SELECT DISTINCT ON (e.emp_no) e.emp_no, 
+		e.first_name, 
+		e.last_name,
+		e.birth_date,
+		de.from_date,
+		de.to_date,
+		t.title
+INTO mentorship_eligibility
+from employees as e
+INNER JOIN dept_emp as de
+	on (e.emp_no=de.emp_no)
+INNER JOIN titles as t
+	on (e.emp_no=t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no;
